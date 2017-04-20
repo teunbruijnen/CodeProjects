2459c3581e425f1e2d3d5c9c1f742b18d73f9f56
teunbruijnen <teunbruijnen@hotmail.com>
2017-04-20 13:21:05 +0200

    Fixed bug but another showed up!

--- someQuoteoftheDay.py
+++ someQuoteoftheDay.py
@@ -20,22 +20,21 @@ print("Looking up quotes...") #Loading text
 quotePage = requests.get('https://www.brainyquote.com/quotes_of_the_day.html')
 quotePage.raise_for_status()
 quotePageSoup = bs4.BeautifulSoup(quotePage.text, "html.parser")
-
 AllQuotes = quotePageSoup.find_all('div', class_='bqcpx')
 
-randomQuote = AllQuotes[0].getText()#randint(0,5)].getText()
+randomQuote = AllQuotes[randint(0,5)].getText()
 printFrom = int(randomQuote.index("Day") + 4) #I want to edit the output so I can add some hashtags(twitter only allows 140 characters per tweet).
 randomQuote = randomQuote[printFrom:]
 
 ExistingTweets = api.get_home_timeline(count = 9)
 #Here we check if the quote hasn't been tweeted already.
 while True:
-    already_used = False
+    already_tweeted = False
     for tweets in ExistingTweets:
         if randomQuote[:15] == tweets['text'][:15]:
-            already_used = True
+            already_tweeted = True
             break
-    if already_used:
+    if already_tweeted:
         randomQuote = AllQuotes[randint(0,5)].getText()
         randomQuote = randomQuote[printFrom:]
     else:
@@ -48,17 +47,22 @@ if len(randomQuote) > 110:
     hashTags = ShorthashTags
 elif len(randomQuote) < 110:
     hashTags = LonghashTags
-
 #have to make sure not to duplicate any tags!
 Tag1 = hashTags[randint(0,4)]
 Tag2 = hashTags[randint(0,4)]
-if Tag2 == Tag1:
-    Tag2 = hashTags[randint(0,4)]
+while True:
+    same_tag = False
+    if Tag1 == Tag2:
+        same_tag = True
+        break
+    if same_tag:
+        Tag2 = hashTags[randint(0,4)]
+    else:
+        break
 
 tweetThis = randomQuote  + "%s %s" % (Tag1, Tag2)
 if len(tweetThis) > 140:
     tweetThis = randomQuote #I want to add the option of having only one hashtag if it is possible in stead of removing BOTH of them.
-
-api.update_status(status=tweetThis)
+#api.update_status(status=tweetThis)
 
 print("Tweeted: " + tweetThis)
