23c560644c27909b3fe19e90290cc087eefd7cdf
teunbruijnen <teunbruijnen@hotmail.com>
2017-05-25 22:01:00 +0200

    Added some hashtags

--- getWeatherinRandcity.py
+++ getWeatherinRandcity.py
@@ -17,23 +17,24 @@ api = Twython(keys[1],keys[2],keys[3],keys[4])
 
 print("Checking the weather...") #Loading text
 #todo: Add more cities?
-CityandSite = {'Amsterdam':'https://weather.com/weather/today/l/NLXX0002:1:NL', 'Paris':'https://weather.com/weather/today/l/FRXX0076:1:FR', 'Barcelona':'https://weather.com/weather/today/l/SPXX0015:1:SP', 'London':'https://weather.com/weather/today/l/UKXX0085:1:UK', 'Berlin':'https://weather.com/weather/today/l/GMXX0007:1:GM'}
+CityandSite = {'Amsterdam':'https://weather.com/weather/today/l/NLXX0002:1:NL', 'Paris':'https://weather.com/weather/today/l/FRXX0076:1:FR', 'Barcelona':'https://weather.com/weather/today/l/SPXX0015:1:SP', 'London':'https://weather.com/weather/today/l/UKXX0085:1:UK', 'Berlin':'https://weather.com/weather/today/l/GMXX0007:1:GM', 'Lima':'https://weather.com/weather/today/l/PEXX0011:1:PE', 'Moscow':'https://weather.com/weather/today/l/RSXX0063:1:RS', 'Istanbul':'https://weather.com/weather/today/l/TUXX0014:1:TU', 'New York':'https://weather.com/weather/today/l/USNY0996:1:US', 'Los Angeles':'https://weather.com/weather/today/l/USCA0638:1:US', 'Buenos Aires':'https://weather.com/weather/today/l/ARBA0009:1:AR', 'Alert':'https://weather.com/weather/today/l/CAXX0829:1:CA'}
 
 City = list(CityandSite.keys())
-randomCity = City[randint(0,4)]
-
+randomCity = City[randint(0,(len(City)-1))]
+print(randomCity)
 weatherPage = requests.get(CityandSite[randomCity])
 weatherSoup = bs4.BeautifulSoup(weatherPage.text, "html.parser")
 
 degreesNowF = weatherSoup.find_all(class_='today_nowcard-temp')
+print(degreesNowF)
 sliceF = degreesNowF[0].getText()
 degreesNowC = ((int)(sliceF[:2]) - 32) / 1.8
 
 niceWords = ['nice', 'great', 'good', 'wonderful', 'beautiful']
-hashTags = ['#weather', '#dailyweather', '#bot', '#python', '#programming']
+hashTags = ['#weather', '#dailyweather', '#bot', '#python', '#programming', '#daily', '#data', '#temp', '#temperature']
 hashTags.extend([randomCity])
 
-tweetThis = ("The temperature in %s at this moment is: %s Fahrenheit. Or %s° Celsius. Have a %s day! %s %s #%s" % (randomCity, degreesNowF[0].getText(), str(round(degreesNowC)), niceWords[randint(0,4)], hashTags[randint(0,1)], hashTags[randint(2,4)], hashTags[5]))
+tweetThis = ("The temperature in %s at this moment is: %s Fahrenheit. Or %s° Celsius. Have a %s day! %s %s #%s" % (randomCity, degreesNowF[0].getText(), str(round(degreesNowC)), niceWords[randint(0,4)], hashTags[randint(0,1)], hashTags[randint(2,4)], hashTags[(len(hashTags)-1)]))
 
 api.update_status(status=tweetThis)
 
--- someQuoteoftheDay.py
+++ someQuoteoftheDay.py
@@ -23,7 +23,7 @@ quotePageSoup = bs4.BeautifulSoup(quotePage.text, "html.parser")
 AllQuotes = quotePageSoup.find_all('div', class_='bqcpx')
 
 randomQuote = AllQuotes[randint(0,5)].getText()
-printFrom = int(randomQuote.index("Day") + 4) #I want to edit the output so I can add some hashtags(twitter only allows 140 characters per tweet).
+printFrom = int(randomQuote.index("Day") + 5) #A bug appeared, first it sliced one character too many, now one character too little. Maybe in a while 5 needs to be changed back to 4. I want to edit the output so I can add some hashtags(twitter only allows 140 characters per tweet).
 randomQuote = randomQuote[printFrom:]
 
 ExistingTweets = api.get_home_timeline(count = 9)
@@ -36,28 +36,28 @@ while True:
             break
     if already_tweeted:
         randomQuote = AllQuotes[randint(0,5)].getText()
-        printFrom = int(randomQuote.index("Day") + 4)
+        printFrom = int(randomQuote.index("Day") + 5)#Don't forget to change this slicing as well!
         randomQuote = randomQuote[printFrom:]
     else:
         break
 
-LonghashTags= ['#inspiration', '#motivation', '#getmotivated', '#dailyquote', '#brainyquote']
-ShorthashTags = ['#quote', '#dailyquote', '#quotes', '#python', '#learn']
+LonghashTags= ['#inspiration', '#motivation', '#getmotivated', '#dailyquote', '#brainyquote', '#lifequotes', '#positive', '#successful', '#inspirational']
+ShorthashTags = ['#quote', '#dailyquote', '#quotes', '#python', '#learn', '#life', '#inspire', '#people']
 
-if len(randomQuote) > 110:
+if len(randomQuote) >= 110:
     hashTags = ShorthashTags
-elif len(randomQuote) < 110:
+elif len(randomQuote) <= 110:
     hashTags = LonghashTags
 #have to make sure not to duplicate any tags!(not sure if this is working properly atm...)
-Tag1 = hashTags[randint(0,4)]
-Tag2 = hashTags[randint(0,4)]
+Tag1 = hashTags[randint(0,(len(hashTags)-1))]
+Tag2 = hashTags[randint(0,(len(hashTags)-1))]
 while True:
     same_tag = False
     if Tag1 == Tag2:
         same_tag = True
         break
     if same_tag:
-        Tag2 = hashTags[randint(0,4)]
+        Tag2 = hashTags[randint(0,(len(hashTags)-1))]
     else:
         break
 
